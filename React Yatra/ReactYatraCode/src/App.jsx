8f9859edd86dc6b2ea4a99b7e66b7152db54f1f7
Mithilesh Yadav <mky120799@gmail.com>
2025-03-30 17:53:09 +0530

    starting hook part

--- React Yatra/ReactYatraCode/src/App.jsx
+++ React Yatra/ReactYatraCode/src/App.jsx
@@ -2,17 +2,26 @@ import React from 'react';
 import Button from './Button';
 
 const App = () => { 
-  const handleClickAction = () => {  
-    console.log('clicked inside app component');
-  };
+ const data = [
+  {id:'a', text:'text 1'},
+  {id:'b', text:'text 2'},
+  {id:'c', text:'text 3'},
+  {id:'d', text:'text 4'},
+  {id:'e', text:'text 5'},
+]
 
-  return (
-    <div>
-      <Button handleClick={handleClickAction} something={{ a: 4, b: { c: 5 } }}>
-        click me
-      </Button>
-    </div>
-  );
+const handleClickAction = (e) => {
+  console.log('I was clicked from the parent');
+}
+return  
+ <>
+  <Text>{data[0].text}</Text>
+  <Text>{data[1].text}</Text>
+  <Text>{data[3].text}</Text>
+  <Text>{data[4].text}</Text> 
+ 
+</>
+
 };
 
 export default App;
